165dbfb066ef3f4d049a9f919e1e2841a83caf16
garafu <garafo@gmail.com>
2023-07-15 23:35:57 +0900

    応答を変更

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res, next) => {
-    var name = req.query.name | 'World';
+    var name = req.query.name || 'World';
     res.send(`Hello ${name}!`);
 });
 
